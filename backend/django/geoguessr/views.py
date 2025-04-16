50b371e6f6ae5a4921bb61a6c00c7b0c1f33e964
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-04-16 21:16:35 +0300

    Some fixes

--- backend/django/geoguessr/views.py
+++ backend/django/geoguessr/views.py
@@ -6,11 +6,10 @@ import base64
 
 from django.contrib import messages
 from django.forms import ValidationError
-from django.http import Http404, HttpResponse
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from verify_email.email_handler import ActivationMailManager
 from django.core.paginator import Paginator
 
 from .models import MilitaryPromote, Photo, RecognitionRequest, User
